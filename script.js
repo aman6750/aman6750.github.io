77f0178846c9335cbcb2de3fc864719540665d38
Aman <amanyadav6750@gmail.com>
2022-09-20 00:19:12 +0530

    update

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Problem Solver", "Blogger"],
+        strings: ["Programmer", "Developer", "Problem Solver", "sports Lover"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Problem Solver", "Blogger"],
+        strings: ["Programmer", "Developer", "Problem Solver", "sports Lover"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
